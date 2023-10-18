3512e937d07be2b567282669e7a7c0457f0f93f8
carloschi7 <carloschi23@libero.it>
2023-10-18 10:45:18 +0200

    fixed build.bat

--- build.bat
+++ build.bat
@@ -6,8 +6,9 @@ cd dependencies\C7Engine
 call "build.bat"
 cd ..\..
 
+set main_dir=%cd%
 mkdir build && cd build
-cmake -DENGINE_PATH=%cd%\dependencies\C7Engine -DENGINE_BUILD_PATH=dependencies\C7Engine\build ..
-msbuild MinecraftClone.sln
+cmake -DENGINE_PATH=%main_dir%\dependencies\C7Engine -DENGINE_BUILD_PATH=%main_dir%\dependencies\C7Engine\build -DAPP_NAME=MinecraftClone ..
+msbuild MinecraftClone.sln /property:Configuration=Release 
 cd ..
 
