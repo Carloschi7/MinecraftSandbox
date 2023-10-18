bce74e5ba8795fb7fbec990ba601435612688da5
carloschi7 <carloschi23@libero.it>
2023-10-18 20:03:52 +0200

    adjusted release build

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ make
 cd ..
 
 mkdir release && cd release
-cmake -DENGINE_PATH=$current_path/dependencies/C7Engine -DENGINE_BUILD_PATH=$current_path/dependencies/C7Engine/release ..
+cmake -DENGINE_PATH=$current_path/dependencies/C7Engine -DENGINE_BUILD_PATH=$current_path/dependencies/C7Engine/release -DCMAKE_BUILD_TYPE=Release ..
 make
 cd ..
