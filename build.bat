ee892cb971d690263329aa20f7ad01892e68f0da
carloschi7 <carloschi23@libero.it>
2023-12-16 11:45:51 +0100

    featured standalone mode for the engine

--- build.bat
+++ build.bat
@@ -1,18 +1,29 @@
 @echo off
 
 git submodule update --init
-cd dependencies\C7Engine
-git checkout master
-call "build.bat" NO_ASSIMP
-cd ..\..
 
 set main_dir=%cd%
+set engine_dir="%main_dir%\dependencies\C7Engine"
+
+cd %engine_dir%
+git checkout master
+cd %main_dir%
+
 mkdir build && cd build
 if "%1" == "standalone" (
+	cd %engine_dir%
+	call "build.bat" NO_ASSIMP STANDALONE
+	cd %main_dir%
+	mkdir build
+	cd build	
 	cmake -DENGINE_PATH=%main_dir%\dependencies\C7Engine -DENGINE_BUILD_PATH=%main_dir%\dependencies\C7Engine\build -DSTANDALONE=1 -DAPP_NAME=MinecraftSandbox ..
 ) else (
+	cd %engine_dir%
+	call "build.bat" NO_ASSIMP
+	cd %main_dir%
+	mkdir build
+	cd build
 	cmake -DENGINE_PATH=%main_dir%\dependencies\C7Engine -DENGINE_BUILD_PATH=%main_dir%\dependencies\C7Engine\build -DAPP_NAME=MinecraftSandbox ..
 )
 msbuild MinecraftSandbox.sln /property:Configuration=Release 
-cd ..
-
+cd %main_dir%
